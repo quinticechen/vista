f655bcbc265dc1fa4641300f4d418b7eefcd3cf4
quinticechen <quinticechen@gmail.com>
2025-09-15 19:20:51 +0800

    Update UrlParamVista.tsx

--- src/pages/UrlParamVista.tsx
+++ src/pages/UrlParamVista.tsx
@@ -492,7 +492,7 @@ const UrlParamVista = () => {
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
               />
-              <Button type="submit" className="bg-amber-500 hover:bg-amber-600">
+              <Button type="submit">
                 Search
               </Button>
             </form>
